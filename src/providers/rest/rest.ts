5a3e7aec703f89635aa8fa6830b5eb6fe743730d
sbattou <sofiane.battou@qlik.com>
2018-03-30 19:20:59 -0400

    Consuming a simple public API

---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+
+  apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(public http: HttpClient) {
+  }
+
+  getUsers() {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  addUser(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.apiUrl, JSON.stringify(data))
+        .subscribe(res => {
+          resolve(res);
+        }, (err) => {
+          reject(err);
+        });
+    });
+  }
+
+}
